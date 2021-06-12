7ec6c2f04c69b05e3de4a48c7a4c4637d36bbb28
Solomon <git@solomonvictorino.com>
2021-06-11 18:30:07 -0700

    post-install: fix "the batch file cannot be found"

--- post-install.bat
+++ post-install.bat
@@ -43,4 +43,4 @@
 @usr\bin\bash.exe --norc -c "export PATH=/usr/bin:$PATH; export SYSCONFDIR=/etc; for p in $(export LC_COLLATE=C; echo /etc/post-install/*.post); do test -e \"$p\" && . \"$p\"; done"
 
 @REM Remove this script
-@DEL post-install.bat
+@DEL post-install.bat & EXIT
